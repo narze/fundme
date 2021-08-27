cf499a7467f1f6cfd40a35422bd44a56167278a3
Manassarn Manoonchai <manassarn@gmail.com>
2021-08-27 21:22:49 +0700

    List repos

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require("@actions/core")
+const github = require("@actions/github")
+const wait = require("./wait")
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const ms = core.getInput("milliseconds")
+    core.info(`Waiting ${ms} milliseconds ...`)
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    core.debug(new Date().toTimeString()) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    await wait(parseInt(ms))
+    core.info(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString());
+    core.setOutput("time", new Date().toTimeString())
+
+    const githubToken = core.getInput("githubToken")
+    const octokit = github.getOctokit(githubToken)
+
+    const username = "narze"
+
+    core.info(
+      octokit.rest.repos.listForUser({
+        username,
+      })
+    )
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
